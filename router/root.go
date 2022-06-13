93c3d3c1d8c4be40381a4fb477bc6e2e5c2daac8
italosilva18 <italosilva14@hotmail.com>
2022-06-13 00:18:52 -0300

    [Atualização dos controllers]

--- router/root.go
+++ router/root.go
@@ -7,6 +7,10 @@ import (
 
 func Root(e *echo.Echo) {
 
+	e.POST("/add", controllers.Add)
+	e.GET("/show", controllers.Users.Show)
+	e.PUT("/update", controllers.Users.update)
+	e.DELETE("/delete", controllers.Users.delete)
 	e.GET("/pont", controllers.Get.Novo)
 	e.GET("/2", controllers.Users.Sim)
 }
