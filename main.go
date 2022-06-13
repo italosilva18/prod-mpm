b564019bdd8e15b6cac000b552fb48fcf10ed0f2
italosilva18 <italosilva14@hotmail.com>
2022-06-13 20:54:22 -0300

    [atualização]

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/italosilva18/prod-mpm/config"
 	"github.com/italosilva18/prod-mpm/router"
 	"github.com/labstack/echo/v4"
 )
@@ -8,6 +9,8 @@ import (
 func main() {
 
 	e := echo.New()
+
+	config.ConnectDB()
 
 	router.Root(e)
 
