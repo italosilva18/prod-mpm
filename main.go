991756dbca27b189239027c93077ceb9c11bef9e
italosilva18 <italosilva14@hotmail.com>
2022-06-15 23:53:44 -0300

    [Atualização]

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"github.com/italosilva18/prod-mpm/repository"
 	"github.com/italosilva18/prod-mpm/router"
-	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
 	router.Root(e)
 
